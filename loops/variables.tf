a29bf7e9d471c7a4fe8aa6417d844ec6de35ff16
Soumyakusuma <soumyakusuma2@gmail.com>
2025-06-10 16:54:57 +0530

    nginx install

---
+++ loops/variables.tf
@@ -0,0 +1,65 @@
+variable "ami-id" {
+  type    = string
+  default =  "ami-09c813fb71547fc4f"
+  description = "AMI ID Creation"
+}
+
+
+#variable "instance" {
+  #default = "t3.micro"
+#}
+
+
+variable "ec2-tags" {
+  type=map(string)
+  default =  {
+    name= "HelloWorld"
+}
+}
+
+variable "sg_allow"{
+    default= "allow_all_ips"
+
+}
+
+variable "describe"{
+    default= "allow all ips "
+
+}
+
+variable "from_port"{
+    default= 0
+}
+variable "to_port"{
+    default= 0
+}
+
+variable "cidr"{
+    type= list(string)
+    default= ["0.0.0.0/0"]
+}
+
+variable "sg-tags" {
+  type=map(string)
+  default =  {
+    name= "allow_all_ips"
+}
+}
+
+variable "type_of_env"{
+  default="prod"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
